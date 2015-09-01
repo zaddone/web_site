d44911acdbe0ca0a04a84f3c324b98057aa8add5
sureboy <zaddone@qq.com>
2015-09-01 18:16:14 +0800

    kkk

--- settings_admin.py
+++ settings_admin.py
@@ -122,7 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',

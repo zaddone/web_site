b3615ddf43b33b989c64887f3e0298e14a117d5f
sureboy <zaddone@qq.com>
2015-09-01 16:06:06 +0800

    1112

--- user_center/settings.py
+++ user_center/settings.py
@@ -33,7 +33,7 @@ TIME_ZONE = 'Asia/Shanghai'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'zh-us'
+LANGUAGE_CODE = 'zh-Hans'
 
 
 SITE_ID = 1
@@ -124,7 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',

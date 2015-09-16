b5ee06619949ca8f49c4625fab53c37e99f3882c
sureboy <zaddone@qq.com>
2015-09-16 13:33:30 +0800

    ....

--- website/urls_m.py
+++ website/urls_m.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('website.views',
                        url(r'^find_city/$','find_city'),
                        url(r'^themedetail/$', 'site_theme_page', name='theme_page'),
+                       
                        url(r'^map/(?P<query>[\S\s-]+)/$', 'view_map',{'template_name':'m_showmap.html'}),
+                       url(r'^map/$', 'view_map',{'template_name':'m_showmap.html'}),
                        url(r'^page-(?P<query>[\S\s-]+).html$', 'life_page',{'template_name':'m_showpage.html'}), 
                        url(r'^(?P<query>[\S-]+)/$', 'tag_city_page',{'template_name':'m_showlist.html'}), 
                        url(r'^$', 'tag_city_page',{'template_name':'m_showlist.html'}), 
